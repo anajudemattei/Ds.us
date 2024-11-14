28486b62fb59e1a54bee7c911ddcca2188aff360
Ana Julia Demattei <anajuliademattei26@gmail.com>
2024-11-14 11:32:35 -0300

    feat: criação das classes

---
+++ src/models/jogadores/Jogo.js
@@ -0,0 +1,27 @@
+class jogo {
+    constructor (mapa, missoes, minimoJogadores)
+    {
+        this.mapa = mapa;
+        this.missoes = missoes;
+        this.minimoJogadores = minimoJogadores;
+    }
+} 
+
+class desenvolvedor {
+    constructor(id, nome, envolvimento, caracteristicas, status) {
+        this.id = id;
+        this.nome = nome;
+        this.envolvimento = envolvimento;
+        this. caracteristicas = caracteristicas;
+        this.status = status;
+    }
+}
+
+class sabotador {
+    constructor(id, nome, envolvimento, caracteristicas) {
+        this.id = id;
+        this.nome = nome;
+        this.envolvimento = envolvimento;
+        this. caracteristicas = caracteristicas;
+    }
+}
